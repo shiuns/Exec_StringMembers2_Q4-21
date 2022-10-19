860e2addf3e8916289e297945de5720e0168517f
shiuns <shiunworks@gmail.com>
2022-10-19 23:36:59 +0800

    加入專案檔案。

---
+++ 作業(Exec_StringMembers2_Q4)-21謝孟勳/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 作業_Exec_StringMembers2_Q4__21謝孟勳
+{
+    //假設有表示日期的字串"1100225", 請將它轉成表示西元年的日
+    //期"20210225"
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //不會使用TaiwanCalendar
+        }
+    }
+}
